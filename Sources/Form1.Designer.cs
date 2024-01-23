f835384023cc1a66ad93153bfd7def3d19886164
Ragndow-xia-qian <372378529@qq.com>
2024-01-23 22:49:22 +0800

    优化项目结构，添加标题，设置窗口大小

---
+++ Sources/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace GoogologyQuest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.title.Font = new System.Drawing.Font("华文彩云", 30F);
+            this.title.ForeColor = System.Drawing.Color.Red;
+            this.title.Location = new System.Drawing.Point(70, -4);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(656, 92);
+            this.title.TabIndex = 0;
+            this.title.Text = "Googology Quest";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ Sources/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogologyQuest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Rectangle workingArea = Screen.GetWorkingArea(this); // 获取工作区大小
+
+            // 设置窗口的大小为窗口的 1/4 大小
+            this.Width = workingArea.Width / 2;
+            this.Height = workingArea.Height / 2;
+
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing); // 设置关闭事件
+
+            // 计算窗口应该显示的位置
+            int x = (workingArea.Width - this.Width) / 2;
+            int y = (workingArea.Height - this.Height) / 2;
+
+            this.Location = new Point(x, y); // 设置窗口的位置
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("你确定要退出吗？", "提示", MessageBoxButtons.OKCancel) != DialogResult.OK)
+            {
+                e.Cancel = true; // 取消关闭操作
+            }
+        }
+    }
+}
